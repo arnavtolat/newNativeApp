a74496c44e5da130c33ac62715e2ded02b4a3f06
Arnav Tolat <85039883+arnavtolat@users.noreply.github.com>
2022-02-08 13:57:10 -0500

    Update WelcomeScreen.js

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -58,7 +58,7 @@ function WelcomeScreen(props) {
 
 const styles = StyleSheet.create({
     background: {
-        flex: 2,
+        flex: 7,
         alignItems: "center",
         width: "100%",
         height: "100%"
